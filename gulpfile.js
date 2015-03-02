3dc7db27c4781dcd2678c5212e940c31decc06ef
Jon Gold <hello@designedbygold.com>
2015-03-02 01:04:41 +0000

    V2 initial commit - clean slate

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var basswork = require('gulp-basswork');
+var minifyCss = require('gulp-minify-css');
+var webserver = require('gulp-webserver');
+
+gulp.task('css', function() {
+  gulp.src('./src/css/base.css')
+    .pipe(basswork())
+    .pipe(gulp.dest('./css'))
+    .pipe(minifyCss())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('serve', function() {
+  gulp.src('.')
+    .pipe(webserver({livereload: true}));
+});
+
+gulp.task('default', ['css', 'serve'], function() {
+  gulp.watch(['./src/**/*'], ['css']);
+});
+
